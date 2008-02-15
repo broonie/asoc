4582dffa9204cbedc5b58135b0754b5559a20af1
Mark Brown <broonie@opensource.wolfsonmicro.com>
2008-02-15 11:26:16 +0000

    Remove obsolete sound/driver.h inclusion
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm9713.c
+++ sound/soc/codecs/wm9713.c
@@ -22,7 +22,6 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/device.h>
-#include <sound/driver.h>
 #include <sound/core.h>
 #include <sound/pcm.h>
 #include <sound/ac97_codec.h>
