22f99984b386a4974c6cc5296c682defb355ac0c
Mark Brown <broonie@opensource.wolfsonmicro.com>
2008-05-22 13:21:59 +0100

    ASoC: Remove sound/driver.h inclusions from codecs
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/ad1939.c
+++ sound/soc/codecs/ad1939.c
@@ -32,7 +32,6 @@
 #include <linux/i2c.h>
 #include <linux/platform_device.h>
 #include <linux/workqueue.h>
-#include <sound/driver.h>
 #include <sound/core.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
