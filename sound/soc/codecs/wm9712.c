d08cd97392467a22c778955c364b155dc043891e
Mark Brown <broonie@opensource.wolfsonmicro.com>
2008-06-11 12:50:04 +0100

    ASoC: Fix register cache initialisation for Wolfson AC97
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm9712.c
+++ sound/soc/codecs/wm9712.c
@@ -664,8 +664,8 @@ static int wm9712_soc_probe(struct platform_device *pdev)
 		ret = -ENOMEM;
 		goto cache_err;
 	}
-	codec->reg_cache_size = ARRAY_SIZE(wm9712_reg);
-	codec->reg_cache_step = 1;
+	codec->reg_cache_size = sizeof(wm9712_reg);
+	codec->reg_cache_step = 2;
 
 	codec->name = "WM9712";
 	codec->owner = THIS_MODULE;
