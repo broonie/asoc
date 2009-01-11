cd4a1250dec141be265027035327fce8c3df780a
Mark Brown <broonie@opensource.wolfsonmicro.com>
2009-01-11 20:11:59 +0000

    ASoC: Update WM8974 for current kernel APIs
    Compile tested only.
    
    Signed-off-by: Mark Brown <broonie@opensource.wolfsonmicro.com>

--- sound/soc/codecs/wm8974.c
+++ sound/soc/codecs/wm8974.c
@@ -478,7 +478,8 @@ static int wm8974_set_dai_fmt(struct snd_soc_dai *codec_dai,
 }
 
 static int wm8974_pcm_hw_params(struct snd_pcm_substream *substream,
-	struct snd_pcm_hw_params *params)
+				struct snd_pcm_hw_params *params,
+				struct snd_soc_dai *dai)
 {
 	struct snd_soc_pcm_runtime *rtd = substream->private_data;
 	struct snd_soc_device *socdev = rtd->socdev;
@@ -586,8 +587,6 @@ struct snd_soc_dai wm8974_dai = {
 		.formats = WM8974_FORMATS,},
 	.ops = {
 		.hw_params = wm8974_pcm_hw_params,
-	},
-	.dai_ops = {
 		.digital_mute = wm8974_mute,
 		.set_fmt = wm8974_set_dai_fmt,
 		.set_clkdiv = wm8974_set_dai_clkdiv,
@@ -659,7 +658,7 @@ static int wm8974_init(struct snd_soc_device *socdev)
 	wm8974_set_bias_level(codec, SND_SOC_BIAS_STANDBY);
 	wm8974_add_controls(codec);
 	wm8974_add_widgets(codec);
-	ret = snd_soc_register_card(socdev);
+	ret = snd_soc_init_card(socdev);
 	if (ret < 0) {
 		printk(KERN_ERR "wm8974: failed to register card\n");
 		goto card_err;
@@ -826,8 +825,19 @@ struct snd_soc_codec_device soc_codec_dev_wm8974 = {
 	.suspend = 	wm8974_suspend,
 	.resume =	wm8974_resume,
 };
-
 EXPORT_SYMBOL_GPL(soc_codec_dev_wm8974);
+
+static int __init wm8974_modinit(void)
+{
+	return snd_soc_register_dai(&wm8974_dai);
+}
+module_init(wm8974_modinit);
+
+static void __exit wm8974_exit(void)
+{
+	snd_soc_unregister_dai(&wm8974_dai);
+}
+module_exit(wm8974_exit);
 
 MODULE_DESCRIPTION("ASoC WM8974 driver");
 MODULE_AUTHOR("Liam Girdwood");
